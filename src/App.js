20aca444dc4f9bc75f132fcc23d26c9fb3bba8e8
Timemash <timemash24@gmail.com>
2022-07-27 15:24:12 +0900

    Update code to client credential flow

--- src/App.js
+++ src/App.js
@@ -1,68 +1,78 @@
 import React from 'react';
 import axios from 'axios';
+import qs from 'qs';
 import { useEffect, useState } from 'react';
 
 function App() {
   const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
   const ARTIST_ID = '3Nrfpe0tUJi4K4DXYWgMUX';
-  const REDIRECT_URI = 'http://localhost:3000';
-  const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
-  const RESPONSE_TYPE = 'token';
 
-  const [token, setToken] = useState('');
-  const [searchKey, setSearchKey] = useState('');
+  // const [searchKey, setSearchKey] = useState('');
   const [albums, setAlbums] = useState([]);
 
-  // const getToken = () => {
-  //     let urlParams = new URLSearchParams(window.location.hash.replace("#","?"));
-  //     let token = urlParams.get('access_token');
-  // }
+  const auth_token = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
+
+  const getAuth = async () => {
+    try {
+      //make post request to SPOTIFY API for access token, sending relavent info
+      const token_url = 'https://accounts.spotify.com/api/token';
+
+      // body data to be x-www-form-urlencoded but axios is converting the body to json
+      const form = qs.stringify({ grant_type: 'client_credentials' });
+
+      const response = await axios.post(token_url, form, {
+        headers: {
+          Authorization: `Basic ${auth_token}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
+
+      return response.data.access_token;
+    } catch (error) {
+      //on fail, log the error in console
+      console.log(error);
+    }
+  };
+  const testing = async (index) => {
+    //request token using getAuth() function
+    const access_token = await getAuth();
+
+    const api_url = `https://api.spotify.com/v1/artists/${ARTIST_ID}/albums/`;
+
+    try {
+      const response = await axios.get(api_url, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+        params: {
+          inlcude_groups: 'album',
+          market: 'KR',
+          limit: 50,
+          offset: index * 50,
+        },
+      });
+      const data = await response.data.items;
+
+      console.log(data);
+      setAlbums([...albums, ...data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const hash = window.location.hash;
-    let token = window.localStorage.getItem('token');
-
-    // getToken()
-
-    if (!token && hash) {
-      token = hash
-        .substring(1)
-        .split('&')
-        .find((elem) => elem.startsWith('access_token'))
-        .split('=')[1];
-
-      window.location.hash = '';
-      window.localStorage.setItem('token', token);
-    }
-
-    setToken(token);
+    testing(0);
+    testing(1);
+    console.log(albums);
   }, []);
 
-  const logout = () => {
-    setToken('');
-    window.localStorage.removeItem('token');
-  };
-
-  const searchAlbums = async (e) => {
-    e.preventDefault();
-    const { data } = await axios.get(
-      `https://api.spotify.com/v1/artists/${ARTIST_ID}/albums`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    setAlbums(data.items);
-  };
-
   const renderAlbums = () => {
-    console.log(albums);
+    // console.log(albums);
     return albums.map((album) => (
       <div key={album.id}>
         {album.images.length ? (
-          <img src={album.images[1].url} alt="" />
+          <img src={album.images[2].url} alt="" />
         ) : (
           <div>No Image</div>
         )}
@@ -74,26 +84,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Spotify React</h1>
-        {!token ? (
-          <a
-            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-          >
-            Login to Spotify
-          </a>
-        ) : (
-          <button onClick={logout}>Logout</button>
-        )}
-
-        {token ? (
-          <form onSubmit={searchAlbums}>
-            <button type={'submit'}>Show Albums</button>
-          </form>
-        ) : (
-          <h2>Please login</h2>
-        )}
-
-        {renderAlbums()}
+        <h1>Spotify API - load albums</h1>
+        {/* {renderAlbums()} */}
       </header>
     </div>
   );
